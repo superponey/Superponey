9c2f054065e15c6a794026c247aec96f5c09d677
superponey <poney@laposte.net>
2012-10-30 11:51:21 +0100

    add French translation

--- TestGit/src/com/poney/test/MyNewClass.java
+++ TestGit/src/com/poney/test/MyNewClass.java
@@ -1,10 +1,24 @@
 package com.poney.test;
 
+import java.util.Locale;
+
 public class MyNewClass
 {
 
 	public String getMessage()
 	{
 		return "Hello git";
+	}
+
+	public String getLocalizedMessage( Locale locale )
+	{
+		if ( Locale.FRENCH.equals( locale ) )
+		{
+			return "Bonjour git";
+		}
+		else
+		{
+			return this.getMessage();
+		}
 	}
 }
